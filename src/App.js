ed1c75b9d5529302f254cbe716a2b746d46ed019
Nechir dev <66407817+Nechir-89@users.noreply.github.com>
2022-07-21 09:08:15 -0400

    seting up lifecycle method to update state every second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import './App.css'
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeAndDate: new Date()
+    }
+  }
 
-function App() {
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.setState({ timeAndDate: new Date() })
+    }, 1000);
+  }
 
-  return null;
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { timeAndDate } = this.state;
+
+    return <div className="wrapper">
+      <div className="time">
+        <span>{('0' + timeAndDate.getHours()).slice(-2)}</span>:
+        <span>{('0' + timeAndDate.getMinutes()).slice(-2)}</span>:
+        <span>{('0' + timeAndDate.getSeconds()).slice(-2)}</span>
+        <div className="amOrPm">
+          { }
+        </div>
+      </div>
+      <div className="timezone">
+        {
+          findTimezone(timeAndDate)
+        }
+      </div>
+    </div>
+  }
 }
-
-
-
+function findTimezone(date) {
+  let dateString = date.toString();
+  let index = dateString.indexOf('(')
+  return dateString.slice(index + 1, dateString.length - 1)
+}
 export default App;
